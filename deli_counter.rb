2e0aa88844b3ce40fe248fb8b26c4f180efc6e95
Ben Hogan <j.benhogan@gmail.com>
2020-06-18 15:06:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line(line_array)
+  line_length = line_array.size
+  line_list = "The line is currently: "
+  if line_length == 0
+    puts "The line is currently empty."
+  else
+    line_array.each_with_index do |person, index|
+      line_list = line_array.join("#{index+1}. #{person} ")
+    end
+    puts line_list
+  end
+end
+
+def take_a_number(array, person)
+  array << person
+end
+
+def now_serving(array, person)
+end
