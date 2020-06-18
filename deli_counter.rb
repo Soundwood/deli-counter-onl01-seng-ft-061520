b7756415c2f53113be6e377d3c0c2fccb0e20642
Ben Hogan <j.benhogan@gmail.com>
2020-06-18 15:24:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,8 @@ def line(line_array)
 end
 
 def take_a_number(array, person)
-  puts "Welcome, #{person}. You are number #{array.size+1} in line."
+  array << person
+  puts "Welcome, #{person}. You are number #{array.size} in line."
 end
 
 def now_serving(array, person)
